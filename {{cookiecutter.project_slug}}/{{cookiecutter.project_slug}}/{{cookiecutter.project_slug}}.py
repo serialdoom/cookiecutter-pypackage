ced300b9b5d0cb6bd6bd71c86c6e6459dc4747e1
Mike <serialdoom@users.noreply.github.com>
2016-06-13 16:14:04 +0100

    added version function for main

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -2,4 +2,17 @@
 
 
 def get_version():
-    return '{{ cookiecutter.version }}'
+    return '{{ cookiecutter.version }}' 
+
+def main_version():
+    """ main function to return version if required """
+    import argparse
+    this_parser = argparse.ArgumentParser(description='')
+    this_parser.add_argument('-V', '--version',
+                             action='version',
+                             version='%(prog)s ' + get_version(),
+                             help='Return the current version')
+    dummy_args = this_parser.parse_args()  # NOQA
+
+if __name__ == '__main__':
+    main_version()
